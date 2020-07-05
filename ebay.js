f14461e4c488f2d12a1c51489ff4bbe3fb153cf6
Basith Kunimal <basith@intellicar.in>
2020-07-05 21:59:42 +0530

    initial

---
+++ ebay.js
@@ -0,0 +1,20 @@
+const Axios = require('axios');
+const cheerio = require('cheerio');
+
+exports.fetchData = async (productid) => {
+    const result = await Axios.get('https://www.ebay.com/itm/' + productid);
+    const $ = cheerio.load(result.data);
+    const title = $('title').text().split('|')[0];
+    const url = $('meta[property="og:url"]').attr('content')
+    const img = $('#icImg').attr('src')
+    const thumbnails = []
+    $('#vi_main_img_fs').find($('.tdThumb img')).each(function(f) {
+        thumbnails.push($(this).attr('src'))
+    })
+    return {
+        title,
+        url,
+        img,
+        thumbnails,
+    }
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const { fetchData } = require('./ebay');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+	next();
+});
+
+
+const sendRsp = (res) => {
+	return function(err, data, msg) {
+		if(err) res.status(400).send({status:'FAILURE', data: null, err, msg});
+		else res.status(200).send({status:'SUCCESS', data, err: null, msg});
+	}
+}
+
+app.get('/api/findproduct/:productid', (req, res) => {
+	const cb = sendRsp(res);
+	fetchData(req.params.productid).then(data => {
+		cb(null, data, 'Product fetch successful');
+	})
+});
+
+app.listen(4040);
